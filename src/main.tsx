fe1ed58ee8b96fd5bfbc1f98f86a2977840e05fd
CinderDeFi <55252584+CinderDeFi@users.noreply.github.com>
2026-05-18 11:43:42 -0400

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary'
+import { ToastProvider } from './components/ui/Toast'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </ErrorBoundary>
   </React.StrictMode>
 )
